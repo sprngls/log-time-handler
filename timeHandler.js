834691c50f0a43531043297c1c31a840b88271de
Springles <contact@springles.xyz>
2025-05-28 21:34:44 +0200

    Update timeHandler.js

--- timeHandler.js
+++ timeHandler.js
@@ -3,7 +3,7 @@ module.exports = function getTime() {
         hour: new Date().getHours().toString().padStart(2, '0'),
         minute: new Date().getMinutes().toString().padStart(2, '0'),
         seconds: new Date().getSeconds().toString().padStart(2, '0'),
-        date: new Date().getDate().toString() + new Date().getMonth().toString() + new Date().getFullYear().toString()
+        date: new Date().getDate().toString() + '-' + new Date().getMonth().toString() + '-' + new Date().getFullYear().toString()
     }
 
     const finalString = `[${timeObject.hour}:${timeObject.minute}:${timeObject.seconds}-${timeObject.date}]`;
